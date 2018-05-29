495a2730c57abccd49667b037b694df1be468984
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-05-29 13:08:59 -0700

    importing and adding components to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import Current from './Components/Current';
+import WantTo from './Components/WantTo';
+import Read from './Components/Read';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <Current />
+        <WantTo />
+        <Read />
       </div>
     );
   }
