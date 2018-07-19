f7a67e3c10bb9c915f1f46a222c05de22a9107a0
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-07-19 10:53:01 -0700

    rendering list of books from BooksAPI.js to Currently reading Component

--- src/Components/Current.js
+++ src/Components/Current.js
@@ -1,14 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Current extends Component {
-	render() {
+const Current = ({book}) =>  {
+	if(!book) {
 		return (
 			<div className="Current">
 				<h3>Currently Reading</h3>
+				<div>Loading...</div>
 				<hr />
 			</div>
-		);
+			)
 	}
-}
 
+	return (
+		<div className="Current">
+			<h3>Currently Reading</h3>
+			{book.map((el) => {
+				return (
+					<li key={el.id} id="booksList">
+						<img src={el.imageLinks.thumbnail} alt={el.title}/>
+						<select id="select">
+							<option></option>
+							<option>Move To...</option>
+							<option>Currently Reading</option>
+							<option>Want To Read</option>
+							<option>Read</option>
+						</select>
+					</li>
+					);
+			})}
+			<hr />
+		</div>
+	);
+
+}
 export default Current;
