b2437316f2b61a948de6046704ff77785c907591
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-07-19 10:51:27 -0700

    fetching data from BooksAPI.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,40 @@ import Header from './Components/Header';
 import Current from './Components/Current';
 import WantTo from './Components/WantTo';
 import Read from './Components/Read';
+import Search from './Components/Search';
+import * as BooksAPI from './BooksAPI';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      search: []
+    }
+  }
+  componentDidMount() {
+    BooksAPI.getAll()
+    .then(response => {
+      this.setState({books: response});
+      console.log(this.state.books)
+    })
+
+    BooksAPI.search('tree').then(response => {
+      this.setState({search: response}); 
+      console.log(response);
+    })
+
+  }
+
   render() {
+
+  const list = this.state.books;
     return (
       <div className="App">
         <Header />
-        <Current />
+        <Search />
+        <Current book={list}/>
         <WantTo />
         <Read />
       </div>
