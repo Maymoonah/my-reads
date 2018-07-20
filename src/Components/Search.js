53881afba2b77c67b75f345a970d0743f416e49c
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-07-19 17:37:23 -0700

    fetch books using input and search api and render results to component

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,24 +1,60 @@
 import React, { Component } from 'react';
+import * as BooksAPI from '../BooksAPI';
+import WantTo from './WantTo';
 
 class Search extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			input: '',
-			books: [],
-			booksArray: []
+			query: '',
+			results: []
 		}
 	}
-	
+
+	onChange(e) {
+		this.setState({query: e});
+
+		BooksAPI.search(this.state.query)
+		.then(response => {
+			this.setState({results: response});
+		});
+	}
+
+	submit(el) {
+		console.log(el);
+	}
+
 	render() {
+		const result = this.state.results;
+
+		if(!result) {
+			return (
+				<div className="search">
+					<div className="input">
+						<input type="text" value={this.state.query} placeholder="Search Books..." onChange={(e)=>this.onChange(e.target.value)}></input>
+						<button onClick={() =>this.submit(this.state.results)}><i className="fas fa-search"></i></button>
+					</div>
+					<div id="searchResults">
+						Loading...
+					</div>
+				</div>
+			);
+		}
+
 		return (
 			<div className="search">
 				<div className="input">
-					<input type="text" value={this.state.input} placeholder="Search Books..." onChange={(e)=>this.onChange(e.target.value)}></input>
-					<button onClick={()=>this.submit(this.state.input)}><i className="fas fa-search"></i></button>
+					<input type="text" value={this.state.query} placeholder="Search Books..." onChange={(e)=>this.onChange(e.target.value)}></input>
+					<button onClick={() =>this.submit(this.state.results)}><i className="fas fa-search"></i></button>
 				</div>
-				<div className="booksList">
-					{this.state.booksArray.map((val)=><div className="divName"  key={this.state.booksArray[val]}>{val}</div>)}
+				<div id="searchResults">
+					{result.map((res) => {
+						return (
+							<li key={res.id} className="booksList">
+								<img src={res.imageLinks.smallThumbnail} />
+							</li>
+						);
+					})}
 				</div>
 			</div>
 		);
